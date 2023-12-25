833d1161fc4253d78e7c5f351a0f4aab817d3934
Naman Vaishnav <namanmvaishnav@gmail.com>
2023-12-25 10:27:41 +0530

    Initial Commit

---
+++ APIConsumer/APIConsumerApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIConsumerApp.swift
+//  APIConsumer
+//
+//  Created by Naman Vaishnav on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct APIConsumerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
